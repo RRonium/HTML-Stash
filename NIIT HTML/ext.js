f632fbc55325d2c0519c846a616bf575ecbc6037
RRonium <sannid2105@gmail.com>
2026-04-10 03:09:56 +0530

    first commit

---
+++ NIIT HTML/ext.js
@@ -0,0 +1,3 @@
+alert('Script executed');
+alert('I am Sannidhya');
+document.write('Hello World');
---
+++ NIIT HTML/sale.js
@@ -0,0 +1,3 @@
+alert("Product on sale:\n" +" 1. LEO Mobile \n" +" 2. LEO Camera\n" +'3. Red shoes \n'+" 4. KP Watch \n")
+alert('I am '+'Sannidhya');   
+alert('The Sum is :'+(10+20));
